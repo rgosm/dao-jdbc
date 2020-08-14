a44236067aabf1ada231d6baccfd7fab6f78047b
Rogerio <rgo_sm@hotmail.com>
2020-08-14 02:29:55 -0300

    findById implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -10,6 +10,6 @@ public interface SellerDao {
     void insert(Seller obj);
     void update(Seller obj);
     void deleteById(Integer id);
-    Department findById(Integer id);
+    Seller findById(Integer id);
     List<Seller> findAll();
 }
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -7,7 +7,7 @@ public class Department implements Serializable {
     private Integer id;
     private String name;
 
-    public Department(Integer id) {
+    public Department() {
     }
 
     public Department(Integer id, String name) {
