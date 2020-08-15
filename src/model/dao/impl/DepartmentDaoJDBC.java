a922f2e58e4c87d30cca6f4e01af8600e4539010
Rogerio <rgo_sm@hotmail.com>
2020-08-15 01:23:15 -0300

    Insert Department Implementation

---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,64 @@
+package model.dao.impl;
+
+import db.DB;
+import db.dbException;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+import java.sql.*;
+import java.util.List;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+    private Connection conn;
+
+    public DepartmentDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
+    @Override
+    public void insert(Department obj) {
+        PreparedStatement st = null;
+
+        try{
+            st = conn.prepareStatement(
+                    "INSERT INTO department(Name) VALUES(?)", Statement.RETURN_GENERATED_KEYS);
+            st.setString(1, obj.getName());
+            int rowsAffected = st.executeUpdate();
+            if(rowsAffected > 0){
+                ResultSet rs = st.getGeneratedKeys();
+                if(rs.next()){
+                    int id = rs.getInt(1);
+                    obj.setId(id);
+                }
+                DB.closeResultSet(rs);
+            }else{
+                throw new dbException("Unexpected error! No one rows affected");
+            }
+
+        }catch (SQLException e){
+            throw new dbException(e.getMessage());
+        }finally {
+            DB.closeStatement(st);
+        }
+    }
+
+    @Override
+    public void update(Department obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return null;
+    }
+}
